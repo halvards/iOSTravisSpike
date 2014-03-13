2dd7fdd798e51f2abd1fb2dce03a3cb6daa7c42e
mlam <mlam@thoughtworks.com>
2014-03-13 15:36:33 +1100

    Empty application

---
+++ iOSTravisSpike/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSTravisSpike
+//
+//  Created by Mai K Lam on 13/03/2014.
+//  Copyright (c) 2014 mlam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOSTravisSpike/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSTravisSpike
+//
+//  Created by Mai K Lam on 13/03/2014.
+//  Copyright (c) 2014 mlam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
